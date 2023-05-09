3b7dcc8f6ac74ba2c4195e8a8e20930029b89b49
Nick Cho <nicholas.cho@hotmail.ca>
2023-05-09 08:46:11 -0400

    Abstracting account information into a separate component

--- web3-live-demo/src/App.js
+++ web3-live-demo/src/App.js
@@ -1,45 +1,25 @@
 import React, { useEffect} from 'react'
 import Button from '@mui/material/Button'
 import Grid from '@mui/material/Grid'
-import Typography from '@mui/material/Typography'
+
+import DisplayInformation from './components/DisplayInformation'
 
 import {  useEtherBalance, useEthers, useBlockMeta } from '@usedapp/core'
-import { formatEther } from '@ethersproject/units'
-
 
 export default function App() {
   const { account, deactivate, activateBrowserWallet, chainId, library} = useEthers()
   const etherBalance = useEtherBalance(account)
   const blockInfo = useBlockMeta();
 
-  //deleted check for if config.readOnlyUrls is populated
-  useEffect(() => {
-
-    console.log(library)
-    }, [account, etherBalance])
     // 'account' being undefined means that we are not connected.
   return (
       <Grid container spacing ={2}>
         <Grid item xs = {5}></Grid>
-        <Grid item backgroundColor="black" sx={{maxHeight:"100%"}} borderRadius="25px" xs = {3} mt={20} sx={{justifyContent: 'center'}}>
+        <Grid item backgroundColor="black" sx={{maxHeight:"100%", justifyContent:"center"}} borderRadius="25px" xs = {3} mt={20}>
           {!account && <Button variant = "contained" color="success" onClick={() => activateBrowserWallet()}>Connect</Button>}
           {account && <Button variant = "contained" color="success"onClick={() => deactivate()}>Disconnect</Button>}
           
-          {etherBalance && account && (
-            <div className="balance">
-              <br />
-              <Typography color="white" variant="h4">Chain ID</Typography>
-              <p>{chainId}</p>
-              <Typography color="white" variant="h4">Block Number</Typography>
-              <p>{blockInfo.blockNumber}</p>  
-              <Typography color="white" variant="h4">Wallet Address:</Typography>
-              <p className="bold">{account}</p>
-              <Typography color="white" variant="h4">Wallet Balance (Ether):</Typography>
-              <p className="bold">{formatEther(etherBalance)}</p>
-              <Typography color="white" variant="h4">Wallet Balance (Wei):</Typography>
-              <p>{formatEther(etherBalance)** 10}</p>
-            </div>
-          )}
+          <DisplayInformation blk={blockInfo} id={chainId}  acc={account} balance = {etherBalance}/>   
       </Grid>
     </Grid>
   )
---
+++ web3-live-demo/src/components/DisplayInformation.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { formatEther } from '@ethersproject/units'
+
+import Typography from '@mui/material/Typography'
+function DisplayInformation (props) {
+  const balance = props.balance
+  return (
+    <>
+      {balance && props.acc && (
+        <div className="balance">
+          <br />
+          <Typography color="white" variant="h4">Chain ID</Typography>
+          <p>{props.id}</p>
+          
+          <Typography color="white" variant="h4">Block Number</Typography>
+          <p>{props.blk.blockNumber}</p>  
+          
+          <Typography color="white" variant="h4">Wallet Address:</Typography>
+          <p className="bold">{props.acc}</p>
+          
+          <Typography color="white" variant="h4">Wallet Balance (Ether):</Typography>
+          <p className="bold">{formatEther(balance)}</p>
+          
+          <Typography color="white" variant="h4">Wallet Balance (Wei):</Typography>
+          <p>{formatEther(balance)** 10}</p>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default DisplayInformation
